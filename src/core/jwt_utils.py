6f6ef825ffb518626b12b860834a6385234e1542
Станислав Рубцов <frex@mail.ru>
2025-02-25 22:00:05 +0300

    edit jwt_utils(add docstring)

--- src/core/jwt_utils.py
+++ src/core/jwt_utils.py
@@ -2,18 +2,17 @@ from datetime import datetime, timedelta, timezone
 
 import bcrypt
 import jwt
-from fastapi import Response
 
-from src.core.config import COOKIE_NAME, setting, setting_conn
+from src.core.config import setting, setting_conn
 
 
 def create_hash_password(password: str) -> bytes:
     """
     Создание хеш пароля
-    :param password: str
-        пароль
-    :return: bytes
-        хеш значение пароля
+    :param password: пароль
+    :type password: str
+    :rtype: bytes
+    :return: хеш значение пароля
     """
     salt = bcrypt.gensalt()
     pwd_bytes: bytes = password.encode()
@@ -26,12 +25,12 @@ def validate_password(
 ) -> bool:
     """
     Проверка валидности пароля. Проверяет пароль с хеш-значением правильного пароля
-    :param password: str
-        переданный пароль
-    :param hashed_password: bytes
-        хеш-значение правильного пароля
-    :return: bool
-        возвращает True, если пароль верный иначе - False
+    :param password: переданный пароль
+    :type password: str
+    :param hashed_password: хеш-значение правильного пароля
+    :type hashed_password: bytes
+    :rtype: bool
+    :return: возвращает True, если пароль верный иначе - False
     """
     return bcrypt.checkpw(
         password=password.encode(),
@@ -43,17 +42,18 @@ def encode_jwt(
     payload: dict,
     key: str = setting_conn.SECRET_KEY,
     algorithm: str = setting.auth_jwt.algorithm,
-):
+) -> str:
     """
      Создает jwt-токена по алгоритму RS256 (с использованием ассиметричных ключей)
-    :param payload: dict
-        содержание jwt-токена
-    :param key: str
-        секретный ключ
-    :param algorithm: str
-        задается алгоритм
-    :return:
-        возвращает jwt-токен
+    :param payload: содержание jwt-токена
+    :type payload: dict
+    :param key: секретный ключ
+    :type key: str
+    :param algorithm: задается алгоритм
+    :type algorithm: str
+    :rtype: str
+    :return: возвращает jwt-токен
+
     """
     encoded = jwt.encode(payload, key, algorithm=algorithm)
     return encoded
@@ -65,21 +65,28 @@ def decode_jwt(
     algorithm: str = setting.auth_jwt.algorithm,
 ):
     """
-        Раскодирует jwt-токен
-    :param token: str | bytes
-        jwt-токен
-    :param key: str
-        секретный ключ шифрования
-    :param algorithm: str
-        алгоритм шифрования
-    :return:
-        ?? содержание токена (payload)
+    Раскодирует jwt-токен
+    :param token: jwt-токен
+    :type token: str | bytes
+    :param key: секретный ключ шифрования
+    :type key: str
+    :param algorithm: алгоритм шифрования
+    :type algorithm: str
+    :rtype: dict
+    :return: содержание токена (payload)
     """
     decoded = jwt.decode(token, key, algorithms=[algorithm])
     return decoded
 
 
 def create_jwt(user: str) -> str:
+    """
+    Создание jwt-токен
+    :param user: данные пользователя
+    :type user: str
+    :rtype: str
+    :return: jwt-токен
+    """
     payload = dict()
     payload["sub"] = user
     expire = datetime.now(timezone.utc) + timedelta(
