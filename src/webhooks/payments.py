02384fd27013e2f8de06c58fa6b0ece4d3cb1415
Станислав Рубцов <frex@mail.ru>
2025-02-25 21:40:27 +0300

    edit webhooks.payments(add comments)

--- src/webhooks/payments.py
+++ src/webhooks/payments.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
+from src.users.schemas import OutUserSchemas
+
 router = APIRouter()
 
 
 # Создание документации с описанием данных, которые отдает WEBHOOK
 # здесь info не данные запроса, а структура данных ответа на другой сервер по WEBHOOK'у
 @router.post("payments-processing")
-# TODO описать модель ответа
-async def payments_processing(info: int):
+# info - данные(тип данных) которые мы передаем по webhook
+async def payments_processing(info: OutUserSchemas):
     pass
