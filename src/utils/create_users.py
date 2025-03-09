58c8e81b5996d0c1f0e47aa5a3c1ceda2ae8e4df
Станислав Рубцов <frex@mail.ru>
2025-03-09 11:26:14 +0300

    edit create_users(insert find user)

--- src/utils/create_users.py
+++ src/utils/create_users.py
@@ -1,26 +1,46 @@
 import asyncio
+import logging
+
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+
 from src.core.database import async_session_maker
 from src.users.models import User
 from src.users.crud import create_user
 from src.users.schemas import UserCreateSchemas
+from src.core.config import configure_logging
+
+configure_logging(logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 async def create_new_users() -> None:
     """
     Утилита по созданию новых пользователей
     """
+    logger.info("Start create default users")
     async with async_session_maker() as session:
-        user_admin: UserCreateSchemas = UserCreateSchemas(
-            full_name="Петров Иван", email="admin@mycomp.com", password="1qaz!QAZ"
-        )
-        admin: User = await create_user(session=session, user_data=user_admin)
-        admin.is_superuser = True
-        await session.commit()
+        stmt = select(User).filter(User.email == "admin@mycomp.com")
+        res: Result = await session.execute(stmt)
+        user: User = res.scalar_one_or_none()
+        if user is None:
+            logger.info("Users creation completed")
+            user_admin: UserCreateSchemas = UserCreateSchemas(
+                full_name="Петров Иван", email="admin@mycomp.com", password="1qaz!QAZ"
+            )
+            admin: User = await create_user(session=session, user_data=user_admin)
+            admin.is_superuser = True
+            await session.commit()
 
-        user: UserCreateSchemas = UserCreateSchemas(
-            full_name="Ветлицкий Сергей", email="user1@mycomp.com", password="2wsx@WSX"
-        )
-        res: User = await create_user(session=session, user_data=user)
+            user: UserCreateSchemas = UserCreateSchemas(
+                full_name="Ветлицкий Сергей",
+                email="user1@mycomp.com",
+                password="2wsx@WSX",
+            )
+            res: User = await create_user(session=session, user_data=user)
+            logger.info("Users have already been created")
+        else:
+            logger.info("Users have already been created")
 
 
 if __name__ == "__main__":
